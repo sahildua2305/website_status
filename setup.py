d2afc07a508fb988b82db32d3e4dcff6663a984a
sahildua2305 <sahildua2305@gmail.com>
2015-07-18 00:34:43 +0530

    Updated to 0.0.5

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name='website_status',
-      version='0.0.1',
-      description='Check whether a website is up or down with just one function call',
+      version='0.0.5',
+      description='Check whether a website is up or down',
+      long_description='Check whether a website is up or down with just one function call',
+      keywords='website status website down website up',
       url='http://github.com/sahildua2305/website_status',
       author='Sahil Dua',
       author_email='sahildua2305@gmail.com',
--- website_status/__init__.py
+++ website_status/__init__.py
@@ -1,3 +1,8 @@
+# Author{
+# 	name: sahildua2305
+# 	url : http://sahildua.com
+# }
+
 import requests
 
 headers = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36"
@@ -30,12 +35,21 @@ def website_up(url=""):
 # 	False: if website is up
 # }
 def website_down(url=""):
+	return not website_up(url)
+
+# function to check whether a website is down or not
+# params{
+# 	url: Website url to be checked
+# }
+# returns{
+# 	-1: if website is invalid or connection timed out
+# 	10x or 20x or 30x or 40x or 50x HTTP standard code if requests returns a valid HTTP code
+# }
+def website_code(url=""):
 	if url=="":
-		return True
+		return -1
 	try:
 		response = requests.get(url, headers={'user_agent': headers}, timeout=15)
-		if response.status_code==200:
-			return False
-		return True
-	except requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema:
-		return True
+		return response.status_code
+	except requests.exceptions.Timeout, requests.exceptions.ConnectionError:
+		return -1
